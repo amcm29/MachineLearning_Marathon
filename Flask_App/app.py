6ca64e40e88aa98bd70815513897fbb02be933d8
Data Analyst 77002 <analyst77002@outlook.com>
2019-05-16 03:31:40 -0500

    lastest app.py

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,108 +1,134 @@
 # import necessary libraries
-import os
-import io
-import numpy as numpy
-
+import numpy as np
+import pandas as pd
 from flask import Flask, request, redirect, url_for, jsonify, render_template
-
 import pickle
 from sklearn.externals import joblib
-
 import datetime
+
+with open(f'model_5K.pkl', 'rb') as f:
+    model_5K = pickle.load(f)
 
 app = Flask(__name__)
 
-model_5K = None
-
-def load_model_5K():
-    # Load the model from the file
-    global model_5K
-    model_5K = joblib.load('model_5K.pk1')
-load_model_5K()
-
-model_10K = None
-
-def load_model_10K():
-    # Load the model from the file
-    global model_10K
-    model_10K = joblib.load('model_10K.pk1')
-load_model_10K()
-
-model_15K = None
-
-def load_model_15K():
-    # Load the model from the file
-    global model_15K
-    model_15K = joblib.load('model_15K.pk1')
-load_model_15K()
-
-model_20K = None
-
-def load_model_20K():
-    # Load the model from the file
-    global model_20K
-    model_20K = joblib.load('model_20K.pk1')
-load_model_20K()
-
-model_Half = None
-
-def load_model_Half():
-    # Load the model from the file
-    global model_Half
-    model_Half = joblib.load('model_Half.pk1')
-load_model_Half()
-
-model_25K = None
-
-def load_model_25K():
-    # Load the model from the file
-    global model_25K
-    model_25K = joblib.load('model_25K.pk1')
-load_model_25K()
-
-model_30K = None
-
-def load_model_30K():
-    # Load the model from the file
-    global model_30K
-    model_30K = joblib.load('model_30K.pk1')
-load_model_30K()
-
-model_35K = None
-
-def load_model_35K():
-    # Load the model from the file
-    global model_35K
-    model_35K = joblib.load('model_35K.pk1')
-load_model_35K()
-
-model_40K = None
-
-def load_model_40K():
-    # Load the model from the file
-    global model_40K
-    model_40K = joblib.load('model_40K.pk1')
-load_model_40K()
-
-model_Final = None
-
-def load_model_Final():
-    # Load the model from the file
-    global model_Final
-    model_Final= joblib.load('model_Final.pk1')
-load_model_Final()
-
-
-
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        # Just render the initial form, to get input
+        return(render_template('index.html'))
 
-@app.route('/send', methods=['GET', 'POST'])
-def send():
+
     if request.method == 'POST':
-        bib_number = int(request.form['inputBib'])
+        # Extract the input
+        bib = int(request.form['inputBib'])
         age = int(request.form['inputAge'])
+        gender = int(request.form['inputGroupSelector01'])
+            
+        temperature = int(request.form['inputTemp'])
+        timeh = int(request.form['inputHours'])
+        timeHsec=timeh*3600
+        timem = int(request.form['inputMinutes'])
+        timeMsec=timem*60
+        inputTime = (timeMsec + timeHsec)
+
+
+
+
+        input_variables = pd.DataFrame([[bib, age, gender, temperature, inputTime]],
+                                        columns=['bib', 'age', 'gender','temperature', 'inputTime'],
+                                        dtype=int)
+                                        # index=['input'])
+
+        # model_5K = None
+
+        def model_5K():
+            # Load the model from the file
+            global model_5K
+            model_5K = joblib.load('model_5K.pk1')
+        prediction5K = model_5K.predict(input_variables)
+
+        results_5K = str(datetime.timedelta(seconds=round(prediction5K)))
+
+        return render_template('index.html',
+                                    data[0]=results_5K,
+                                    )
+
+
+
+# model_10K = None
+
+# def load_model_10K():
+#     # Load the model from the file
+#     global model_10K
+#     model_10K = joblib.load('model_10K.pk1')
+# prediction10K = model_10K.predict(user_10K_data)
+
+# model_15K = None
+
+# def load_model_15K():
+#     # Load the model from the file
+#     global model_15K
+#     model_15K = joblib.load('model_15K.pk1')
+# prediction15K = model_15K.predict(user_15K_data)
+
+# model_20K = None
+
+# def load_model_20K():
+#     # Load the model from the file
+#     global model_20K
+#     model_20K = joblib.load('model_20K.pk1')
+# prediction20K = model_20K.predict(user_20K_data)
+
+# model_Half = None
+
+# def load_model_Half():
+#     # Load the model from the file
+#     global model_Half
+#     model_Half = joblib.load('model_Half.pk1')
+# predictionHalf = model_Half.predict(user_half_data)
+
+# model_25K = None
+
+# def load_model_25K():
+#     # Load the model from the file
+#     global model_25K
+#     model_25K = joblib.load('model_25K.pk1')
+# prediction25K = model_25K.predict(user_25K_data)
+
+# model_30K = None
+
+# def load_model_30K():
+#     # Load the model from the file
+#     global model_30K
+#     model_30K = joblib.load('model_30K.pk1')
+# prediction30K = model_30K.predict(user_30K_data)
+
+# model_35K = None
+
+# def load_model_35K():
+#     # Load the model from the file
+#     global model_35K
+#     model_35K = joblib.load('model_35K.pk1')
+# prediction35K = model_35K.predict(user_35K_data)
+
+# model_40K = None
+
+# def load_model_40K():
+#     # Load the model from the file
+#     global model_40K
+#     model_40K = joblib.load('model_40K.pk1')
+# prediction40K = model_40K.predict(user_40K_data)
+
+# model_Final = None
+
+# def load_model_Final():
+#     # Load the model from the file
+#     global model_Final
+#     model_Final= joblib.load('model_Final.pk1')
+# predictionFinal = model_Final.predict(user_final_data)
+
+
+
         # group1 = int(request.form['inputGroupSelect01'])
         
     # if group1 = int('inputGroupSelect01'): 
@@ -111,40 +137,15 @@ def send():
     #     genderm = '0'
             
     #     if group1 = 'Female' then genderf = '1' elif genderf = '0'
-        temperature = float(request.form['inputTemp'])
-        timeh = int(request.form['inputHours'])
-        timem = int(request.form['inputMinutes'])
-        form_data = {
-            'Bib': int(bib_number),
-            'Age': int(age),
-            'receivedHours': int(timeh),
-            'receivedMinutes': int(timem),
             # 'F': int(genderf),
             # 'M': int(genderm),
-            'Temp (F)': int(temperature)
-        }
 
-seconds_in_hour = 3600
-seconds_in_minute = 60
 
-seconds = int(input('Enter a number of seconds: '))
 
-hours = seconds // seconds_in_hour
-seconds = seconds - (hours * seconds_in_hour)
-
-minutes = seconds // seconds_in_minute
-seconds = seconds - (minutes * seconds_in_minute)
-
-# concat('{1:.0f} hours, {2:.0f} minutes, {3:.0f} seconds.'.format(hours, minutes, seconds))
 
 # df2=pd.DataFrame({'Bib':[25000], 'Age':[42],'Official Time Duration':[22175], 'F':[0], 'M':[1],'Temp (F)':[65]})
 # model_5K.predict(df2)
 
-# datatime = str(datetime.timedelta(seconds = (data5)))
-
-
-
-dataList = []
 
 
 # # Use the loaded model to make predictions
@@ -171,10 +172,6 @@ dataList = []
 # dataList.append.data35
 # dataList.append.data40
 # dataList.append.Final
-
-        
-# if request.method == 'GET':
-#    return render_template('index.html')
 
 
 if __name__ == '__main__':
