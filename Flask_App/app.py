cf723fda41fd7ba26c5b54c004c4dd83fc8cd31f
Data Analyst 77002 <analyst77002@outlook.com>
2019-05-17 13:55:43 -0500

    updated of app.py and HTML

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -32,15 +32,15 @@ def send():
         timem = int(request.form['inputMinutes'])
         timeMsec=timem*60
         inputTime = (timeMsec + timeHsec)
-        if request.form['inputGroupSelect01'] == '1':
+        if request.form['inputGroupSelect01'] == "1":
             F = 0
             M = 1
         else:
             F = 1
             M = 0
-
+        print(request.form['inputGroupSelect01'])
         temperature = int(request.form['inputTemp'])
-        input_variables = pd.DataFrame([[bib, age,  inputTime, F, M, temperature]],
+        input_variables5K = pd.DataFrame([[bib, age,  inputTime, F, M, temperature]],
                                         columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)'],
                                         dtype=int)
                                         # index=['input'])
@@ -52,131 +52,159 @@ def send():
             # global model_5K
             # model_5K = LinearRegression()
         model_5K = joblib.load('model/model_5K.pkl')
-        prediction5K = model_5K.predict(input_variables)
+        prediction5K = model_5K.predict(input_variables5K)
 
         stage_5K = datetime.timedelta(seconds=int(prediction5K[0]))
         print('hi')
         print(stage_5K)
-        return render_template('index.html', data=stage_5K)
 
 
 # model_10K = None
 
-# def load_model_10K():
-#     # Load the model from the file
-#     global model_10K
-#     model_10K = joblib.load('model_10K.pkl')
-# prediction10K = model_10K.predict(user_10K_data)
+        input_variables10K = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_10K = joblib.load('model/model_10K.pkl')
+        prediction10K = model_10K.predict(input_variables10K)
+
+        stage_10K = datetime.timedelta(seconds=int(prediction10K[0]))
+        print('hi')
+        print(stage_10K)
+        # return render_template('index.html', data5K = stage_5K, data10K = stage_10K)
 
 # model_15K = None
 
-# def load_model_15K():
-#     # Load the model from the file
-#     global model_15K
-#     model_15K = joblib.load('model_15K.pkl')
-# prediction15K = model_15K.predict(user_15K_data)
+        input_variables15K = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K, prediction10K]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K', '10K'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_15K = joblib.load('model/model_15K.pkl')
+        prediction15K = model_15K.predict(input_variables15K)
+
+        stage_15K = datetime.timedelta(seconds=int(prediction15K[0]))
+        print('hi')
+        print(stage_15K)
+        # return render_template('index.html', data15K=stage_15K)
 
 # model_20K = None
 
-# def load_model_20K():
-#     # Load the model from the file
-#     global model_20K
-#     model_20K = joblib.load('model_20K.pkl')
-# prediction20K = model_20K.predict(user_20K_data)
+        input_variables20K = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K, prediction10K, prediction15K]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K', '10K', '15K'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_20K = joblib.load('model/model_20K.pkl')
+        prediction20K = model_20K.predict(input_variables20K)
+
+        stage_20K = datetime.timedelta(seconds=int(prediction20K[0]))
+        print('hi')
+        print(stage_20K)
+        # return render_template('index.html', data20K=stage_20K)
 
 # model_Half = None
 
-# def load_model_Half():
-#     # Load the model from the file
-#     global model_Half
-#     model_Half = joblib.load('model_Half.pkl')
-# predictionHalf = model_Half.predict(user_half_data)
+        input_variablesHalf = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K, prediction10K, prediction15K, prediction20K]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K', '10K', '15K', '20K'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_Half = joblib.load('model/model_Half.pkl')
+        predictionHalf = model_Half.predict(input_variablesHalf)
+
+        stage_Half = datetime.timedelta(seconds=int(predictionHalf[0]))
+        print('hi')
+        print(stage_Half)
+        # return render_template('index.html', dataHalf=stage_Half)
 
 # model_25K = None
 
-# def load_model_25K():
-#     # Load the model from the file
-#     global model_25K
-#     model_25K = joblib.load('model_25K.pkl')
-# prediction25K = model_25K.predict(user_25K_data)
+        input_variables25K = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K, prediction10K, prediction15K, prediction20K, predictionHalf]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K', '10K', '15K', '20K', 'Half'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_25K = joblib.load('model/model_25K.pkl')
+        prediction25K = model_25K.predict(input_variables25K)
+
+        stage_25K = datetime.timedelta(seconds=int(prediction25K[0]))
+        print('hi')
+        print(stage_25K)
+        # return render_template('index.html', data25K=stage_25K)
 
 # model_30K = None
 
-# def load_model_30K():
-#     # Load the model from the file
-#     global model_30K
-#     model_30K = joblib.load('model_30K.pkl')
-# prediction30K = model_30K.predict(user_30K_data)
+        input_variables30K = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K, prediction10K, prediction15K, prediction20K, predictionHalf, prediction25K]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K', '10K', '15K', '20K', 'Half', '25K'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_30K = joblib.load('model/model_30K.pkl')
+        prediction30K = model_30K.predict(input_variables30K)
+
+        stage_30K = datetime.timedelta(seconds=int(prediction30K[0]))
+        print('hi')
+        print(stage_30K)
+        # return render_template('index.html', data5K = stage_5K, data10K = stage_10K, data15K = stage_15K, data20K = stage_20K, dataHalf = stage_Half, data25K = stage_25K, data30K = stage_30K)
 
 # model_35K = None
 
-# def load_model_35K():
-#     # Load the model from the file
-#     global model_35K
-#     model_35K = joblib.load('model_35K.pkl')
-# prediction35K = model_35K.predict(user_35K_data)
+        input_variables35K = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K, prediction10K, prediction15K, prediction20K, predictionHalf, prediction25K, prediction30K]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K', '10K', '15K', '20K', 'Half', '25K', '30K'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_35K = joblib.load('model/model_35K.pkl')
+        prediction35K = model_35K.predict(input_variables35K)
+
+        stage_35K = datetime.timedelta(seconds=int(prediction35K[0]))
+        print('hi')
+        print(stage_35K)
+        # return render_template('index.html', data5K = stage_5K, data10K = stage_10K, data15K = stage_15K, data20K = stage_20K, dataHalf = stage_Half, data25K = stage_25K, data30K = stage_30K, data35K = stage_35K)
 
 # model_40K = None
 
-# def load_model_40K():
-#     # Load the model from the file
-#     global model_40K
-#     model_40K = joblib.load('model_40K.pkl')
-# prediction40K = model_40K.predict(user_40K_data)
+        input_variables40K = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K, prediction10K, prediction15K, prediction20K, predictionHalf, prediction25K, prediction30K, prediction35K]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K', '10K', '15K', '20K', 'Half', '25K', '30K', '35K'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_40K = joblib.load('model/model_40K.pkl')
+        prediction40K = model_40K.predict(input_variables40K)
+
+        stage_40K = datetime.timedelta(seconds=int(prediction40K[0]))
+        print('hi')
+        print(stage_40K)
+        # return render_template('index.html', data40K=stage_40K)
 
 # model_Final = None
 
-# def load_model_Final():
-#     # Load the model from the file
-#     global model_Final
-#     model_Final= joblib.load('model_Final.pkl')
-# predictionFinal = model_Final.predict(user_final_data)
+        input_variablesFinal = pd.DataFrame([[bib, age,  inputTime, F, M, temperature, prediction5K, prediction10K, prediction15K, prediction20K, predictionHalf, prediction25K, prediction30K, prediction35K, prediction40K]],
+                                        columns=['Bib', 'Age', 'Official Time Duration', 'F', 'M','Temp (F)', '5K', '10K', '15K', '20K', 'Half', '25K', '30K', '35K', '40K'],
+                                        dtype=int)
+                                        # index=['input'])
+
+
+        model_Final = joblib.load('model/model_Final.pkl')
+        predictionFinal = model_Final.predict(input_variablesFinal)
+
+        stage_Final = datetime.timedelta(seconds=int(predictionFinal[0]))
+        print('hi')
+        print(stage_Final)
+        return render_template('index.html', data5K = stage_5K, data10K = stage_10K, data15K = stage_15K, data20K = stage_20K, dataHalf = stage_Half, data25K = stage_25K, data30K = stage_30K, data35K = stage_35K, data40K = stage_40K, dataFinal = stage_Final)
 
 
 
-        # group1 = int(request.form['inputGroupSelect01'])
-        
-    # if group1 = int('inputGroupSelect01'): 
-    #     genderm = '1';
-    # elif:
-    #     genderm = '0'
-            
-    #     if group1 = 'Female' then genderf = '1' elif genderf = '0'
-            # 'F': int(genderf),
-            # 'M': int(genderm),
-
-
-
-
-# df2=pd.DataFrame({'Bib':[25000], 'Age':[42],'Official Time Duration':[22175], 'F':[0], 'M':[1],'Temp (F)':[65]})
-# model_5K.predict(df2)
-
-
-
-# # Use the loaded model to make predictions
-# data5 = model_5K_from_joblib.predict()
-# data10 = model_10K_from_joblib.predict()
-# data15 = model_15K_from_joblib.predict()
-# data20 = model_20K_from_joblib.predict()
-# dataHalf = model_Half_from_joblib.predict()
-# data25 = model_25K_from_joblib.predict()
-# data30 = model_30K_from_joblib.predict()
-# data35 = model_35K_from_joblib.predict()
-# data40 = model_40K_from_joblib.predict()
-# dataFinal = model_Final_from_joblib.predict()
-
-
-
-# dataList.append.data5
-# dataList.append.data10
-# dataList.append.data15
-# dataList.append.data20
-# dataList.append.dataHalf
-# dataList.append.data25
-# dataList.append.data30
-# dataList.append.data35
-# dataList.append.data40
-# dataList.append.Final
 
 
 if __name__ == '__main__':
